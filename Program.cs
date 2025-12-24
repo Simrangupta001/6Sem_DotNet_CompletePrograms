8685a1a05c12d829b566d8fdcddc6a7045ae1ce6
Simran Gupta <guptaroyal191@gmail.com>
2025-12-24 10:18:01 +0000

    dotnet lab from 1Q to 10Q

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a list of integers
+        List<int> numbers = new List<int> { 9, 3, 5, 1, 7, 2 };
+
+        // LINQ query to sort in ascending order
+        var sortedNumbers = from n in numbers
+                            orderby n
+                            select n;
+
+        Console.WriteLine("Sorted numbers in ascending order:");
+
+        foreach (int num in sortedNumbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
